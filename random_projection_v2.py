91ed81ebcd33ecb21c52a35bf26662641f1c5dd3
duliwencynthia <cynthia.w.du@gmail.com>
2025-04-28 16:04:25 -0400

    fix multiple labels

--- random_projection_v2.py
+++ random_projection_v2.py
@@ -455,15 +455,11 @@ if __name__ == "__main__":
     #sentences = df_train["text"].tolist() + df_test["text"].tolist()
     sentences = df_train["text"].tolist()
     labels = []
-    for cats in df_train["cats"]:
-        first_label = None
-        for k, v in cats.items():
+    for _, va in df_train["cats"].items():
+        for k, v in va.items():
             if v == 1.0:
-                first_label = k
+                labels.append(k)
                 break
-        labels.append(first_label)
-
-    assert len(sentences) == len(labels)
     # for _, va in df_test["cats"].items():
     #     for k, v in va.items():
     #         if v == 1.0:
@@ -472,7 +468,6 @@ if __name__ == "__main__":
     # Convert labels to IDs
     labels_id = []
     for lb in labels:
-        print(lb)
         labels_id.append(labels_dic[lb])
 
     #print("number of labels:", len(labels_id))
