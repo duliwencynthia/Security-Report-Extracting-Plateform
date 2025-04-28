df3fe6c69c858db4983f22b62839099666d5055a
duliwencynthia <cynthia.w.du@gmail.com>
2025-04-28 15:53:00 -0400

    fix path

--- Smote_Roberta.py
+++ Smote_Roberta.py
@@ -73,6 +73,18 @@ class CustomRobertaClassifier(nn.Module):
             logits=logits,
         )
 
+class TextDataset(Dataset):
+    def __init__(self, texts, labels):
+        self.encodings = tokenizer(texts, truncation=True, padding=True, max_length=256)
+        self.labels = labels
+
+    def __getitem__(self, idx):
+        item = {key: torch.tensor(val[idx]) for key, val in self.encodings.items()}
+        item["labels"] = torch.tensor(self.labels[idx])
+        return item
+
+    def __len__(self):
+        return len(self.labels)
 
 class EmbeddingDataset(Dataset):
     def __init__(self, embeddings, labels):
